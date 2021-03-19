fbc9e07eb2966edad1ecd14c378b3161319b257b
591867443@qq.com <591867443@qq.com>
2021-03-19 10:50:44 +0800

    3.19 完成

--- app/src/main/java/com/example/helloworld/ObjectAnimActivity.java
+++ app/src/main/java/com/example/helloworld/ObjectAnimActivity.java
@@ -2,7 +2,10 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ObjectAnimActivity extends AppCompatActivity {
@@ -13,8 +16,30 @@ public class ObjectAnimActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_object_anim);
-        tvTest=findViewById(R.id.tv);
+        tvTest = findViewById(R.id.tv);
 
-        tvTest.animate().translationYBy(500).setDuration(2000).start();
+        //平移
+//        tvTest.animate().translationYBy(500).setDuration(2000).start();
+
+        //渐变
+//        tvTest.animate().alpha(0).setDuration(2000).start();
+
+//        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+//        valueAnimator.setDuration(2000);
+//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator animation) {
+//                //valueAnimator 实际的值
+//                Log.d("aaaa", animation.getAnimatedValue() + "");
+//                //动画的进度0-1
+//                Log.d("aaaa", animation.getAnimatedFraction() + "");
+//            }
+//        });
+//        valueAnimator.start();
+
+        //
+        ObjectAnimator objectAnimator =ObjectAnimator.ofFloat(tvTest,"translationY",0,500,200,800);
+        objectAnimator.setDuration(2000);
+        objectAnimator.start();
     }
 }
